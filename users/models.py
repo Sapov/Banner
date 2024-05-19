312d04e1ba86ed8e481139302dccdf21a6fcc008
Sapov <sapov@mail.ru>
2024-05-19 11:41:13 +0300

    add users AbstractuserModel

---
+++ users/models.py
@@ -0,0 +1,19 @@
+from django.contrib.auth.models import AbstractUser
+from django.db import models
+
+
+class Role(models.TextChoices):
+    """Роли пользователей"""
+
+    CUSTOMER_RETAIL = "CUSTOMER_RETAIL", "Клиент"  # розничный клиент
+    CUSTOMER_AGENCY = "CUSTOMER_AGENCY", "Рекламное агентство"  # Рекламное агентство
+    MANAGER = "MANAGER", "Менеджер"
+    OPERATOR = "OPERATOR", "Оператор"
+    FINANCIER = "FINANCIER", "Бухгалтер"
+
+
+class User(AbstractUser):
+    role = models.CharField(
+        max_length=24, choices=Role.choices, default=Role.CUSTOMER_RETAIL
+    )
+    phone = models.CharField(max_length=16, verbose_name="Телефон")
